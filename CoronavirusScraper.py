fa3a6ccfc15e3208ef88054d368a623dee5bf3f8
Greer Goodman <ggoodman@mapcom.com>
2020-03-22 14:19:31 -0400

    Added file for the scraper. Will come back to this later

---
+++ CoronavirusScraper.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+
+"""
+Eventually make this an args call maybe?
+"""
+
+
+def get_request():
+    page = requests.get("https://www.buzzfeed.com/")
+    return page.content
+
+
+def parse_soup(content):
+    soup = BeautifulSoup(content, "html.parser")
+    return soup
+
+
+def get_headlines(soup):
+    headlines = soup.find_all("a", class_="js-card__link")
+    coronaCount = 0
+    covidCount = 0
+    virusCount = 0
+    for str in list(headlines):
+        if "corona" in str.get_text():
+            coronaCount += 1
+        elif "covid" in str.get_text():
+            covidCount += 1
+        elif "virus" in str.get_text():
+            virusCount += 1
+    print(
+        "Count of Corona: %s \n Count of Covid: %s \n Count of Virus %s"
+        % (coronaCount, covidCount, virusCount)
+    )
+
+
+content = get_request()
+soup = parse_soup(content)
+get_headlines(soup)
